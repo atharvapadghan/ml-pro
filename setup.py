0d61884d0c6b046d90b0b99cbece4e89c90d4e16
Atharva Padghan <padghanatharva383@gmail.com>
2024-07-17 17:59:04 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYFEN_DOT_E="-e ."
+def get_requirements(file_path:str)-> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", " ")for req in requirements]
+        
+        if HYFEN_DOT_E in requirements:
+            requirements.remove(HYFEN_DOT_E)
+            
+    return requirements
+    
+    
+    
+    
+setup(name="ML-1", VERSION="0.0.1", 
+      author="Atharva Padghan", 
+      author_email="padghanatharva383@gmail.com", 
+      packages=find_packages(), 
+      install_requires=get_requirements("requirement.txt")
+      )
