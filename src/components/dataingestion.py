ef963bec109babcd3a695879c5feabeeccb52750
Atharva Padghan <padghanatharva383@gmail.com>
2024-07-19 19:16:50 +0530

    Fifth Commit

--- src/components/dataingestion.py
+++ src/components/dataingestion.py
@@ -26,7 +26,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered Data Ingestion Method or Component")
         try:
-            df=pd.read_csv("ml-pro/notebook/data/stud.csv")
+            df=pd.read_csv("./notebook/data/stud.csv")
             logging.info("Read the Data Set in DataFrame")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             
